4dbe1e5c074011ff41ae1cd473d1dca8bbd83a5c
elkillooo <diegosanchezleon@gmail.com>
2019-01-25 09:08:56 +0100

    Añade constructor

--- src/tarea2git/diego/Jugador.java
+++ src/tarea2git/diego/Jugador.java
@@ -13,5 +13,12 @@ public class Jugador {
     String nombre;	// Nombre del jugador de fútbol
     int votos;	// Votos recibidos en la aplicación	
     int id;		// Número único identificativo
+
+    public Jugador(String nombre, int votos, int id) {
+        this.nombre = nombre;
+        this.votos = votos;
+        this.id = id;
+    }
+    
     
 }
